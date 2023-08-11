ef1e2e96e3bce4be87fb1ff889cc2281ba5633e0
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-11 19:48:15 -0300

    Add files via upload

---
+++ TP6-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void swap(int *a, int *b);
+
+int main() {
+	int x1, x2;
+	printf("Ingrese x1\n");
+	scanf("%d", &x1);
+	printf ("Ingrese x2\n");
+	scanf("%d", &x2);
+	swap (&x1, &x2);
+	printf("x1=%d x2=%d",x1, x2);
+
+	return 0;
+}
+void swap (int *a, int *b){
+	int temp= *b;
+	*b=*a;
+	*a=temp;
+}
