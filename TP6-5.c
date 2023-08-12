cdc62c5052e34798e6c35b21c35105379e1cb41e
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-11 21:08:33 -0300

    Add files via upload

---
+++ TP6-5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+char unir(char cadena1[], char cadena2[], char cadena3[]){
+	int i, j, p;
+	for (i = 0;i<5; i++){
+		cadena3[i] = cadena1[i];
+		if (cadena1[i] == 0)	break;
+	}
+	p = i;
+	for (i = p,j = 0;j <= 5; i++, j++){
+		cadena3[i] = cadena2[j];
+		if (cadena2[j] == 0)	break;
+	}
+	printf("La cadena unida es: \n %s", cadena3);
+}
+int main() {
+char cadena1[5] = "Hola\0";
+char cadena2[5] = "Chau\0";
+char cadena3[10];
+unir(cadena1, cadena2, cadena3);
+	return 0;
+}
+
