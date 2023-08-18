76e59f4c2fc95222ee67f6e5a95ce878807461d7
avrusf <136859745+avrusf@users.noreply.github.com>
2023-08-18 18:42:14 -0300

    Add files via upload

---
+++ TP6-6.c
@@ -0,0 +1,29 @@
+/*Realizar una función que reciba como argumentos, una letra y una cadena de 
+caracteres y devuelva la cantidad de veces en que aparece esa letra en la cadena.*/
+
+#include <stdio.h>
+int contador=0;
+
+char cadena(char letra, char frase[20]){
+	for(int i=0; i<20; i++){
+		if(letra==frase[i]){
+			contador++;
+		}
+	}
+}
+
+int main() {
+	char letra;
+	char frase[20];
+	printf("Ingrese una letra: ");
+	scanf(" %s", &letra);
+	printf("Ingrese una frase: ");
+	scanf(" %s", &frase);
+	
+	cadena(letra, frase);
+	printf("La cantidad de veces que se repite esta letra es: %d", contador);
+	//printf(" %s", frase);
+	
+	return 0;
+}
+
